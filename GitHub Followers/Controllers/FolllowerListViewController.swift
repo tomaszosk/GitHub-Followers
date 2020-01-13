4ddc2fdda9c96819c853ae406e6444bee0cae8df
tomaszosk <t.oskroba@icloud.com>
2020-01-13 10:23:20 +0100

    Diffable Data Source implemented

--- GitHub Followers/Controllers/FolllowerListViewController.swift
+++ GitHub Followers/Controllers/FolllowerListViewController.swift
@@ -10,15 +10,22 @@ import UIKit
 
 class FolllowerListViewController: UIViewController {
     
+    enum Section {
+        case main
+    }
+    
     var username: String!
+    var followers: [Follower] = []
+    
     var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
         configureCollectionView()
         getFollowers()
-
+        configureDataSource()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -58,10 +65,29 @@ class FolllowerListViewController: UIViewController {
             
             switch result {
                 case .success(let followers):
-                    print(followers)
+                    self.followers = followers
+                    self.updateData()
+                
                 case .failure(let error):
                     self.presentGFLAlertOnMainThread(title: "Bad stuff happened", message: error.rawValue, buttonTitle: "Ok")
             }
+        }
+    }
+    
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView, cellProvider: { (collectionView, indexPath, follower) -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseID, for: indexPath) as! FollowerCell
+            cell.set(follower: follower)
+            return cell
+        })
+    }
+    
+    func updateData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        DispatchQueue.main.async {
+            self.dataSource.apply(snapshot, animatingDifferences: true)
         }
     }
 }
