818965b1a1496ef8b2788fb79b8efa04d99e0543
tomaszosk <t.oskroba@icloud.com>
2020-01-15 12:51:09 +0100

    Caching downloaded images implemented

--- GitHub Followers/Managers/NetworkManager.swift
+++ GitHub Followers/Managers/NetworkManager.swift
@@ -6,11 +6,12 @@
 //  Copyright © 2020 Tomasz Oskroba. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com/users/"
+    private let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
     
     private init() {
     }
