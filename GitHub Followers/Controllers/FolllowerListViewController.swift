9f46050de31455c846949b422d6f6724d38823c7
tomaszosk <t.oskroba@icloud.com>
2020-01-17 10:00:59 +0100

    EmptyState of Followers added

--- GitHub Followers/Controllers/FolllowerListViewController.swift
+++ GitHub Followers/Controllers/FolllowerListViewController.swift
@@ -63,6 +63,15 @@ class FolllowerListViewController: UIViewController {
                     }
                     
                     self.followers.append(contentsOf: followers)
+                    
+                    if self.followers.isEmpty {
+                        let message = "This user doesn't have any followers. Go follow them 😀."
+                        DispatchQueue.main.async {
+                            self.showEmptyStateView(with: message, in: self.view)
+                        }
+                        return
+                    }
+                    
                     self.updateData()
                 
                 case .failure(let error):
