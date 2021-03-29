96ac1c90ff916052dee6a83fb57a9eb422d38495
tomaszosk <t.oskroba@icloud.com>
2021-03-29 21:02:41 +0200

    SearchViewController populated with custom views

---
+++ GitHub Followers/FavoritesListVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListVC.swift
+//  GitHub Followers
+//
+//  Created by Tomasz Oskroba on 3/29/21.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
