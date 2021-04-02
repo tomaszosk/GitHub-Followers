5cb333c6285cf1bfe9e2a48e448adef0c54b1ee8
tomaszosk <t.oskroba@icloud.com>
2021-04-02 23:21:17 +0200

    Created new model files

---
+++ GitHub Followers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHub Followers
+//
+//  Created by Tomasz Oskroba on 4/2/21.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
