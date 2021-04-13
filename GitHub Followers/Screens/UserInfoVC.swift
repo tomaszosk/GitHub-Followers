f5fba678fa302ebd12df2aa2fe2ac4e2dc35e51c
tomaszosk <t.oskroba@icloud.com>
2021-04-13 20:32:43 +0200

    UserInfoViewController setup

---
+++ GitHub Followers/Screens/UserInfoVC.swift
@@ -0,0 +1,26 @@
+//
+//  UserInfoVC.swift
+//  GitHub Followers
+//
+//  Created by Tomasz Oskroba on 4/13/21.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+    }
+    
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+    
+
+}
