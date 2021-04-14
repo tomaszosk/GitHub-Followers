c048371d7857e047d669d52f0a5d09bc20f40294
tomaszosk <t.oskroba@icloud.com>
2021-04-14 22:41:44 +0200

    User Info Header View Controller set up

---
+++ GitHub Followers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GitHub Followers
+//
+//  Created by Tomasz Oskroba on 4/14/21.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
