5915739766aba35992af5c83baccccfd3210e0c2
tomaszosk <t.oskroba@icloud.com>
2021-04-14 22:01:27 +0200

    Added Secondary Title Label

---
+++ GitHub Followers/Custom views/ViewControllers/GFUserInfoHeaderVC.swift
@@ -0,0 +1,18 @@
+//
+//  GFUserInfoHeaderVC.swift
+//  GitHub Followers
+//
+//  Created by Tomasz Oskroba on 4/14/21.
+//
+
+import UIKit
+
+class GFUserInfoHeaderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
