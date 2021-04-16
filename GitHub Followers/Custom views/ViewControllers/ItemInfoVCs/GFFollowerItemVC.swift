f6a090e105cb7a115753a93692cde2fdde3f82f0
tomaszosk <t.oskroba@icloud.com>
2021-04-16 20:50:59 +0200

    Child Info View Controllers added

---
+++ GitHub Followers/Custom views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -0,0 +1,23 @@
+//
+//  GFFollowerItemVC.swift
+//  GitHub Followers
+//
+//  Created by Tomasz Oskroba on 4/16/21.
+//
+
+import UIKit
+
+class GFFollowerItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .following, withCount: user.following)
+        itemInfoViewTwo.set(itemInfoType: .followers, withCount: user.followers)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+    
+}
---
+++ GitHub Followers/Custom views/ViewControllers/ItemInfoVCs/GFRepoItemVC.swift
@@ -0,0 +1,22 @@
+//
+//  GFRepoItemVC.swift
+//  GitHub Followers
+//
+//  Created by Tomasz Oskroba on 4/16/21.
+//
+
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "GitHub Profile")
+    }
+}
